fa912641dfe717f8ed1e94055433bb0103d6fb83
SWAGAT SWAROOP PARIDA <swagatswaroop@gmail.com>
2024-05-02 22:46:59 +0530

    Added Express

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import cluster from "node:cluster";
-import http from "node:http";
+import express from "express";
 import { availableParallelism } from "node:os";
 import process from "node:process";
 import { getPhillaur } from "./caches/locations/phillaurLocation.js";
@@ -27,120 +27,66 @@ if (cluster.isPrimary) {
     console.log(`worker ${worker.process.pid} died`);
   });
 } else {
-  // Workers can share any TCP connection
-  // In this case it is an HTTP server
-  const server = http.createServer((req, res) => {
-    // Handling different routes/endpoints
-    // Extracting the uniquekey from the URL
-    const urlParts = req.url.split("/");
-    // Extract the uniquekey from the URL
-    const unique = decodeURIComponent(urlParts[urlParts.length - 1]);
-    const uniqueKey = parseInt(unique, 10);
-    if (req.url.startsWith("/phillaur/")) {
-      // Find the data based on the uniquekey
-      const responseData = getPhillaur()[uniqueKey - 1];
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          data: responseData.url,
-        })
-      );
-    } else if (req.url.startsWith("/adampur/")) {
-      // Find the data based on the uniquekey
-      const responseData = getAdampur()[uniqueKey - 1];
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          data: responseData.url,
-        })
-      );
-    } else if (req.url.startsWith("/jalandharcantt/")) {
-      // Find the data based on the uniquekey
-      const responseData = getjalandharCantt()[uniqueKey - 1];
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          data: responseData.url,
-        })
-      );
-    } else if (req.url.startsWith("/jalandharnorth/")) {
-      // Find the data based on the uniquekey
-      const responseData = getjalandharNorth()[uniqueKey - 1];
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          data: responseData.url,
-        })
-      );
-    } else if (req.url.startsWith("/jalandharcenter/")) {
-      // Find the data based on the uniquekey
-      const responseData = getjalandharCenter()[uniqueKey - 1];
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          data: responseData.url,
-        })
-      );
-    } else if (req.url.startsWith("/jalandharwest/")) {
-      // Find the data based on the uniquekey
-      const responseData = getjalandharWest()[uniqueKey - 1];
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          data: responseData.url,
-        })
-      );
-    } else if (req.url.startsWith("/kartapur/")) {
-      // Find the data based on the uniquekey
-      const responseData = getKartarpur()[uniqueKey - 1];
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          data: responseData.url,
-        })
-      );
-    } else if (req.url.startsWith("/shahkot/")) {
-      // Find the data based on the uniquekey
-      const responseData = getShahkot()[uniqueKey - 1];
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          data: responseData.url,
-        })
-      );
-    } else if (req.url.startsWith("/nakodar/")) {
-      // Find the data based on the uniquekey
-      const responseData = getNakodar()[uniqueKey - 1];
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          data: responseData.url,
-        })
-      );
-    } else if (req.url.startsWith("/rushcheck/")) {
-      // Find the data based on the uniquekey
-      const responseData = getRush().find(
-        (element) => element.uniqueKey === uniqueKey
-      );
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          data: responseData.rush,
-        })
-      );
-    } else {
-      res.writeHead(404, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          data: "Not found",
-        })
-      );
-    }
-  });
-
+  const app = express();
   const PORT = process.env.PORT || 8000;
 
-  server.listen(PORT, () => {
+  app.get("/phillaur/:uniqueKey", (req, res) => {
+    const responseData = getPhillaur()[req.params.uniqueKey - 1];
+    res.json({ data: responseData.url });
+  });
+
+  app.get("/adampur/:uniqueKey", (req, res) => {
+    const responseData = getAdampur()[req.params.uniqueKey - 1];
+    res.json({ data: responseData.url });
+  });
+
+  app.get("/kartarpur/:uniqueKey", (req, res) => {
+    const responseData = getKartarpur()[req.params.uniqueKey - 1];
+    res.json({ data: responseData.url });
+  });
+
+  app.get("/shahkot/:uniqueKey", (req, res) => {
+    const responseData = getShahkot()[req.params.uniqueKey - 1];
+    res.json({ data: responseData.url });
+  });
+
+  app.get("/jalandharcantt/:uniqueKey", (req, res) => {
+    const responseData = getjalandharCantt()[req.params.uniqueKey - 1];
+    res.json({ data: responseData.url });
+  });
+
+  app.get("/jalandharnorth/:uniqueKey", (req, res) => {
+    const responseData = getjalandharNorth()[req.params.uniqueKey - 1];
+    res.json({ data: responseData.url });
+  });
+
+  app.get("/jalandharwest/:uniqueKey", (req, res) => {
+    const responseData = getjalandharWest()[req.params.uniqueKey - 1];
+    res.json({ data: responseData.url });
+  });
+
+  app.get("/jalandharcenter/:uniqueKey", (req, res) => {
+    const responseData = getjalandharCenter()[req.params.uniqueKey - 1];
+    res.json({ data: responseData.url });
+  });
+
+  app.get("/nakodar/:uniqueKey", (req, res) => {
+    const responseData = getNakodar()[req.params.uniqueKey - 1];
+    res.json({ data: responseData.url });
+  });
+
+  app.get("/rush/:uniqueKey", (req, res) => {
+    const responseData = getRush().find(
+      (element) => element.uniqueKey === uniqueKey
+    );
+    res.json({ data: responseData.url });
+  });
+
+  app.get("*", (req, res) => {
+    res.status(404).json({ data: "Not found" });
+  });
+
+  app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 }
